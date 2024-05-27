7757bab9aa4808faeb907ed3a2f8cef34673fe84
Дмитрий Зубехин <s1lver.arrow@yandex.ru>
2024-05-27 09:18:47 +0300

    first step

---
+++ page_analyzer/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+
+@app.route('/')
+def index():
+    return 'Hello, Hexlet!'
